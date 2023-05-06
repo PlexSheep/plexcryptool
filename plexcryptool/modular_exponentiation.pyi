81716ed42f9ae40677b61ddab0a2f974e43c8c32
PlexSheep <software@cscherr.de>
2023-05-06 16:00:21 +0200

    removed trash and added stub

---
+++ plexcryptool/modular_exponentiation.pyi
@@ -0,0 +1,10 @@
+"""
+Some basic math functionalities
+"""
+def modular_exponentiation(base: int, exp: int, field: int) -> int:
+    """
+    calculates base^exp in the gallois given gallois field
+
+    Uses iterative squaring to be able to calculate large exponents aswell.
+    """
+    ...
